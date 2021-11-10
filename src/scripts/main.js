d0229e924fba9464660645c8b1ce1cfcfba3bb3d
ChasityBrown <chasity.brown@asurion.com>
2021-11-10 11:52:03 -0500

    login button works with credentials

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,27 @@
 import { GiffyGram } from "./GiffyGram.js"
 import { LoginForm } from "./auth/Login.js"
-
+import { fetchUsers } from "./data/provider.js"
 const applicationElement = document.querySelector(".giffygram")
 
 export const renderApp = () => {
     const user = parseInt(localStorage.getItem("gg_user"))
-
-    if (user) {
-        applicationElement.innerHTML = GiffyGram()
-    } else {
-        applicationElement.innerHTML = LoginForm()
-    }
+    fetchUsers().then(
+        () => {
+            if (user) {
+                applicationElement.innerHTML = GiffyGram()
+            } else {
+                applicationElement.innerHTML = LoginForm()
+            }
+            
+        }
+    )
 }
 //calling function
 renderApp()
+
+applicationElement.addEventListener(
+    "stateChanged",
+    () => {
+        renderApp()
+    }
+)
