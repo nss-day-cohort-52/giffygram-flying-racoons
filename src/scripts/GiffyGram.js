d75b16614f8f0d3840b42ff2b279e8a1644184ae
Ashley Dickey <adickey93@gmail.com>
2021-11-12 13:57:37 -0600

    fixed user filter bug, logout button functional, inbox displaying, message count notification

--- src/scripts/GiffyGram.js
+++ src/scripts/GiffyGram.js
@@ -1,8 +1,10 @@
 import { postForm } from "./feed/PostForm.js"
 import { Posts } from "./feed/PostList.js"
+import { Inbox } from "./friends/DirectMessage.js"
 import { MessageForm } from "./message/MessageForm.js"
 import { Footer } from "./nav/Footer.js"
 import { NavBar } from "./nav/NavBar.js"
+import { getMessages } from "./data/provider.js"
 
 
 const applicationElement = document.querySelector(".giffygram")
@@ -31,9 +33,32 @@ applicationElement.addEventListener("click", clickEvent => {
 })
 
 applicationElement.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "inbox") {
+       const inboxPopUp = document.querySelector(".giffygram__feed")
+       inboxPopUp.innerHTML = Inbox()
+        // applicationElement.dispatchEvent(new CustomEvent("stateChanged"))
+    }
+})
+
+applicationElement.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "logo") {
+       const inboxPopUp = document.querySelector(".giffygram__feed")
+       inboxPopUp.innerHTML = GiffyGram()
+        // applicationElement.dispatchEvent(new CustomEvent("stateChanged"))
+    }
+})
+
+applicationElement.addEventListener("click", clickEvent => {
     if (clickEvent.target.id === "miniMode") {
        const postFormPopUp = document.querySelector(".postForm")
        postFormPopUp.innerHTML = postForm()
         // applicationElement.dispatchEvent(new CustomEvent("stateChanged"))
+    }
+})
+
+applicationElement.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "logout") {
+        localStorage.setItem("gg_user", 0)
+        applicationElement.dispatchEvent(new CustomEvent("stateChanged"))
     }
 })
